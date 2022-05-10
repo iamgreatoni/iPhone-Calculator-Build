9c3710d4e4f7e788e6effe3aa5f4f0f1c5a0e833
Oni John <onijohnoluwaseun@gmail.com>
2022-05-10 23:55:35 +0100

    More update to the JavaScript

--- script.js
+++ script.js
@@ -2,10 +2,53 @@ const hourVar = document.querySelector('.hour');
 const minuteVar = document.querySelector('.minute');
 const displayVar= document.querySelector('.display');
 
+const acVar = document.querySelector('.ac');
+const pmVar = document.querySelector('.pm');
+const percentVar = document.querySelector('.percent');
+
+const divisionVar = document.querySelector('.division');
+const multiplicationVar = document.querySelector('.multiplication');
+const subtractionVar = document.querySelector('.subtraction');
+const additionVar = document.querySelector('.addition');
+const equalVar = document.querySelector('.equal');
+
+const decimalVar = document.querySelector('.decimal');
+
+const number0  = document.querySelector('.number-0');
+const number1 = document.querySelector('.number-1');
+const number2 = document.querySelector('.number-2');
+const number3 = document.querySelector('.number-3');
+const number4 = document.querySelector('.number-4');
+const number5 = document.querySelector('.number-5');
+const number6 = document.querySelector('.number-6');
+const number7 = document.querySelector('.number-7');
+const number8 = document.querySelector('.number-8');
+const number9 = document.querySelector('.number-9');
+
+const numberArray = [
+    number0, 
+    number1, 
+    number2, 
+    number3, 
+    number4, 
+    number5, 
+    number6, 
+    number7, 
+    number8, 
+    number9
+];
 
 //hourVar.textContent = '20';
 
 
+
+
+numberArray.forEach( (i) => {
+    i.addEventListener("click", () => {
+        const currentDisplay = displayVar.innerHTML;
+        displayVar.innerHTML = currentDisplay + i;
+    })
+});
 
 const timeUpdate = (() => {
     const currentTime = new Date();
